cc0a8a7ca44a9d5307b6c54e9d0b6e939393025c
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-19 12:11:34 +0600

    add service user

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,13 +1,10 @@
 import { Request, Response } from "express";
 import { User } from "./user.model";
 import { StatusCodes } from 'http-status-codes';
+import { UserServices } from "./user.services";
 const createUser = async (req: Request, res: Response) => {
     try {
-        const { name, email } = req.body;
-        const user = await User.create({
-            name,
-            email
-        })
+        const user = await UserServices.createUser(req.body);
         res.status(StatusCodes.CREATED).json({
             message: "User Created Successfully",
             user
