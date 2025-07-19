f93e7595300885e990aeee5fc40748eb01109fab
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-19 21:14:34 +0600

    globalerror handler and not found page create

--- src/app/modules/middleswares/globalErrorHandlers.ts
+++ src/app/modules/middleswares/globalErrorHandlers.ts
@@ -1,13 +1,22 @@
 import { NextFunction, Request, Response } from "express";
+import { envVars } from "../config/env";
+import AppError from "../../errorHelpers/AppError";
 
-export const globalErrorHandler = (err : any, res : Response, req : Request, next : NextFunction) => {
-   const statusCode = 500;
-   const message = `Something Went Wrong!!`
-
+export const globalErrorHandler = (err : any, req : Request , res : Response,  next : NextFunction) => {
+   let statusCode = 500;
+   let message = "Something Went Wrong!!"
+   
+   if(err instanceof AppError){
+      statusCode = err.statusCode
+      message = err.message
+   } else if(err instanceof Error) {
+      statusCode = 500;
+      message = err.message
+   }
    res.status(statusCode).json({
     success : false,
     message,
     err,
-    stack : ""
+    stack : envVars.NODE_ENV === "development" ? err.stack : null
    })
 }
