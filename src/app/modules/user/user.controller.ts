7316482046f4e81f91551da97494f0a986fd073b
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-20 18:18:12 +0600

    catch async function create

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,18 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from 'http-status-codes';
 import { UserServices } from "./user.services";
-const createUser = async (req: Request, res: Response, next : NextFunction) => {
-    try {
-        const user = await UserServices.createUser(req.body);
-        res.status(StatusCodes.CREATED).json({
-            message: "User Created Successfully",
-            user
-        })
-    } catch(err : any) {
-        next(err)
-    }
-}
+import { catchAsync } from "../middleswares/catchAsync";
+
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserServices.createUser(req.body);
+    res.status(StatusCodes.CREATED).json({
+        message: "User Created Successfully",
+        user
+    })
+})
+
+const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const users = await UserServices.getAllUsers();
+    res.status(StatusCodes.OK).json({
+        success: true,
+        message: "Users fetched successfully",
+        data: users
+    })
+})
 
 export const UserController = {
-    createUser
+    createUser,
+    getAllUsers
 }
