0bc2ddab745459bae42bd20aac9f2acc14a85a4f
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-24 19:59:46 +0600

    token create some ex change

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -4,6 +4,8 @@ import { AuthController } from "./auth.controller";
 const router = Router();
 
 router.post("/login", AuthController.credentialLogin);
+
+
 router.post("/refresh-token", AuthController.getNewAccessToken);
 
 export const AuthRoutes = router;
