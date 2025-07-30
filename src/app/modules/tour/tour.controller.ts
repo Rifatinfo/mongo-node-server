776ea50f75dabb5477ed75e5b609cb21e5516e62
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-30 20:15:22 +0600

    add all tour and create tour

---
+++ src/app/modules/tour/tour.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../middleswares/catchAsync";
+import { sendResponse } from "../middleswares/sendResponse";
+import { TourService } from "./tour.service";
+
+/** Tour */
+const createTour = catchAsync(async (req: Request, res: Response) => {
+    const result = await TourService.createTour(req.body);
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: 'Tour created successfully',
+        data: result,
+    });
+});
+
+const getAllTours = catchAsync(async (req: Request, res: Response) => {
+
+    const result = await TourService.getAllTours();
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Tours retrieved successfully',
+        data: result.data,
+        meta: result.meta,
+    });
+});
+
+/** Tour Types */
+const createTourType = catchAsync(async (req: Request, res: Response) => {
+    const { name } = req.body;
+    const result = await TourService.createTourType({name});
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: 'Tour type created successfully',
+        data: result,
+    });
+});
+
+
+
+export const TourController = {
+    createTour,
+    createTourType,
+    getAllTours
+};
