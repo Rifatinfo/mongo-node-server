20dd34b3bbf55c96b112313eabc4ff69c24a4873
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-31 21:20:26 +0600

    add meta

--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -32,11 +32,16 @@ const getAllTours = async (query: Record<string, string>) => {
     }
     const tours = await Tour.find(searchArray).find(filter).sort(sort).select(fields).skip(skip).limit(limit);
     const totalTours = await Tour.countDocuments();
+    const totalPage = Math.ceil(totalTours/limit); 
+    const meta = {
+        page : page,
+        limit : limit,
+        total : totalTours,
+        totalPage : totalPage 
+    }
     return {
         data: tours,
-        meta: {
-            total: totalTours
-        }
+        meta: meta
     }
 }
 
