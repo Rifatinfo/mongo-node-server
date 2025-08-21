2420b1c6e57a9800a4e0b77630863e3a882c4d74
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-08-21 22:59:02 +0600

    add multiple image

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -2,10 +2,15 @@ import { Request, Response } from "express";
 import { catchAsync } from "../middleswares/catchAsync";
 import { sendResponse } from "../middleswares/sendResponse";
 import { TourService } from "./tour.service";
+import { ITour } from "./tour.interface";
 
 /** Tour */
 const createTour = catchAsync(async (req: Request, res: Response) => {
-    const result = await TourService.createTour(req.body);
+    const payload: ITour = {
+        ...req.body,
+        images: (req.files as Express.Multer.File[]).map(file => file.path)
+    }
+    const result = await TourService.createTour(payload);
     sendResponse(res, {
         statusCode: 201,
         success: true,
