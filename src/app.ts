54df6b0ea05d937451139195006ec2dc3b08a711
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-08-23 13:08:48 +0600

    add some correction

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import { envVars } from "./app/modules/config/env";
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: envVars.FRONTEND_URL,
     credentials: true
